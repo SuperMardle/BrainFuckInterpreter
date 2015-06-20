45f675cb0b69273475d4618398d3db6323d21976
SuperMardle <matao.mardle@gmail.com>
2015-06-20 22:52:52 +0800

    Update src

--- BFInterpreter.c
+++ BFInterpreter.c
@@ -3,53 +3,37 @@
 
 #define LEN 10000
  
-int main(int argc, char **argv)
-{
-    FILE *input = fopen(argv[1], "r");
+int main() {
+    // source 定义了BF的Hello World!代码
+    char source[] = {"++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."};
     char runtime[LEN] = {0};
-    char *sptr;
-    char *wptr;     // 循环时的指针
+    char *ptr = source;
     int pos = 0;
-    int wflag = 0;  // 循环标记
     int row = 1, col = 0;
-    int wrow, wcol;    // 循环时保存的临时行号，列好
-    while (wflag || fscanf(input, "%c", sptr) != EOF)
-    {
-        if (!wflag) //不在循环中
-            ++col;
-        else
-            ++wcol;
-        switch (*sptr)
-        {
+
+    while(*ptr != '\0') {
+        ++col;
+        switch (*ptr) {
             case '>' :
                 ++pos;
                 break;
             case '<' :
-                if (--pos <0)
-                {
+                if (--pos < 0) {
                     printf("%d : %d : ERROR: Illegal pointer value\n", row, col);
-                    return 1;
+                    return 0;
                 }
                 break;
             case '+' :
                 ++runtime[pos];
-                if (runtime[pos] < 0 || runtime[pos] > 255)
-                {
-                    if (!wflag)
-                        printf("%d : %d : ERROR: Illegal value\n", row, col);
-                    else
-                        printf("%d : %d : ERROR: Illegal value\n", wrow, wcol);
-                    return 1;
+                if (runtime[pos] < 0 || runtime[pos] > 255) {
+                    printf("%d : %d : ERROR: Illegal value\n", row, col);
+                    return 0;
                 }
                 break;
             case '-' :
                 --runtime[pos];
-                if (runtime[pos] < 0 || runtime[pos] > 255)
-                {
-                    if (!wflag)
-                        printf("%d : %d : ERROR: Illegal value\n", row, col);
-                    else
-                        printf("%d : %d : ERROR: Illegal value\n", wrow, wcol);
+                if (runtime[pos] < 0 || runtime[pos] > 255) {
+                    printf("%d : %d : ERROR: Illegal value\n", row, col);
                     return 0;
                 }
                 break;
@@ -60,34 +44,29 @@ int main(int argc, char **argv)
                 runtime[pos]=getchar();
                 break;
             case '[' :
-                if (runtime[pos])
-                    wptr = sptr-1;  // 因为最后会++sptr;
-                else                // runtime[pos] == 0 时候，跳出循环
-                    wflag = 0;
-                wrow = row;
-                wcol = col;
+				if(runtime[pos] == 0) {
+					while(*ptr != ']') {
+						++col;
+						++ptr;
+					}
+				}	
                 break;
             case ']' :
-                sptr = wptr;
-                wflag = 1;          
-                row = wrow;
-                col = wcol;
+				if(runtime[pos] != 0) {
+					while(*ptr != '[') {
+						--col;
+						--ptr;
+					}
+					--col;
+					--ptr;
+				}
                 break;
             case '\n' :
-                if (!wflag)
-                {
-                    ++row;
-                    col = 0;
-                }
-                else
-                {
-                    ++wrow;
-                    wcol = 0;
-                }
+                ++row;
+                col = 0;
                 break;
         }
-        ++sptr;
+        ++ptr;
     }
-    fclose(input);
     return 0;
 }
